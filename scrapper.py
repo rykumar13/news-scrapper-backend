576784e8d9ff76d1520aa87996f5be2410a3af64
Rajiv Kumar <rajivk@gentrack.com>
2021-03-10 01:00:39 +0000

    retrieving article

--- scrapper.py
+++ scrapper.py
@@ -3,7 +3,21 @@ import requests
 
 source = requests.get('https://stuff.co.nz').text
 soup = BeautifulSoup(source, "xml")
-tags = soup.find('app-vertical-article-list')
+article_list = soup.find('app-vertical-article-list')
+
+article_name = article_list.find_all('h3')
+article_links = article_list.find_all('a', href=True)
+
+article_count = len(article_name)
+
+for x in range(article_count):
+    print(article_name[x].text.rstrip())
+    print('https://stuff.co.nz' + article_links[x]['href'])
+    source = requests.get('https://stuff.co.nz' + article_links[x]['href']).text
+    soup = BeautifulSoup(source, "xml")
+    desc = soup.find(class_='sics-component__html-injector sics-component__story__intro sics-component__story__paragraph').text
+    print(desc)
+    print('---')
 
 # find ArticleName
 # articles = tags.find_all('h3')
@@ -24,12 +38,3 @@ tags = soup.find('app-vertical-article-list')
 #     temp = soup2.find(class_='sics-component__html-injector sics-component__story__intro sics-component__story__paragraph').text
 #     print(temp)
 
-# retrieve brief desc
-for a in tags.find_all('a', href=True):
-    article_link = "https://stuff.co.nz" + a['href']
-    print(article_link)
-
-    source2 = requests.get(article_link).text
-    soup2 = BeautifulSoup(source2, "xml")
-    temp = soup2.find(class_='sics-component__html-injector sics-component__story__intro sics-component__story__paragraph').text
-    print(temp)
