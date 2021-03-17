1d017a4b75d5f25ef01eca6298869ccccc58f61e
Rajiv Kumar <rajivk@gentrack.com>
2021-03-17 00:28:11 +0000

    fix api bug

--- api_server.py
+++ api_server.py
@@ -3,11 +3,11 @@ from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
+import db_helper as db
 
 
 class Quotes(Resource):
     def get(self):
-        import db_helper as db
         connection = db.create_connection()
         results = db.get_data(connection)[0][0]
         print(results)
