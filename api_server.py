b9214a79f997b8e3c78e35d7dffc791806c8077f
rykumar13 <rykumar13@gmail.com>
2021-04-04 00:38:09 +0100

    adding basic auth

--- api_server.py
+++ api_server.py
@@ -1,16 +1,19 @@
-from flask import Flask
+from flask import Flask, request, make_response
 from flask_restful import Resource, Api
+import db_helper as db
+import database_creds as creds
 
 app = Flask(__name__)
 api = Api(app)
-import db_helper as db
 
 
 class Quotes(Resource):
     def get(self):
-        connection = db.create_connection()
-        results = db.get_data(connection)[0][0]
-        return results
+        if request.authorization and request.authorization.username == creds.api_username and request.authorization.password == creds.api_password:
+            connection = db.create_connection()
+            results = db.get_data(connection)[0][0]
+            return results
+        return make_response('Could not verify!', 401, {'WWW-Authentication': 'Basic realm="Login Required"'})
 
 
 api.add_resource(Quotes, '/')
