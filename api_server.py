f5ef6ab36b535d8f0b707fdc3698bb5dc6874860
rykumar13 <rykumar13@gmail.com>
2021-04-05 23:30:09 +0100

    adding server config

--- api_server.py
+++ api_server.py
@@ -19,4 +19,4 @@ class Quotes(Resource):
 api.add_resource(Quotes, '/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port='8080')
--- test.py
+++ test.py
@@ -1,11 +1,16 @@
-import db_helper as db
+import requests
 
 
 def main():
-    connection = db.create_connection()
-    results_json = db.get_data(connection)
-    print(results_json)
-    return results_json
+    # url = "http://127.0.0.1:5000/"
+    url = "http://localhost:8080/"
+    raw_response = requests.get(url=url, auth=('api_username', 'api_password'))
+
+    if raw_response.status_code == 200:
+        result = raw_response.json()
+        print(result)
+    else:
+        print(str(raw_response.status_code) + " - " + raw_response.text)
 
 
 if __name__ == "__main__":
